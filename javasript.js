5c5de06a2e23ba5057483306728aa471b63b4da5
Patrick Mahler <patrick_mahler@web.de>
2018-10-12 09:06:24 +0200

    Onload zusammengeführt

--- javasript.js
+++ javasript.js
@@ -2,34 +2,9 @@
 window.onload = function(){
     aktDropDown = document.getElementById("DropDown").value;
     console.log(aktDropDown);
-}
-function aktualisieren(){
-    aktDropDown = document.getElementById("DropDown").value;
-    console.log("Aktualisiert");
-}
-function init(){
-    ZutatenAnz = 3;
-    array = ["1","2","3","4"];
-    for (var i = 0; i <= ZutatenAnz; i++) {
-       array[i] = document.getElementById("00"+(i+1)).innerHTML;
-       console.log(array[i]);
-   }
-    document.getElementById("DropDown").addEventListener('onchange', aendern());
-}
 
-function aendern(){
-    var neuDropDown = document.getElementById("DropDown").value;
-    for (var i = 0; i <= ZutatenAnz; i++) {
-        aktZutatenWert = array[i];
-        neuZutatenWert = ((aktZutatenWert/aktDropDown)* neuDropDown);
-        document.getElementById("00"+(i+1)).innerHTML = neuZutatenWert;
-    }
-    aktualisieren();
-}
 
-window.onload = function(){
-
-    //Check File API support
+    /*LUCAS TEIL*/
     if(window.File && window.FileList && window.FileReader)
     {
         var filesInput = document.getElementById("files");
@@ -86,6 +61,31 @@ window.onload = function(){
     }
 }
 
+
+
+function aktualisieren(){
+    aktDropDown = document.getElementById("DropDown").value;
+    console.log("Aktualisiert");
+}
+function init(){
+    ZutatenAnz = 3;
+    array = ["1","2","3","4"];
+    for (var i = 0; i <= ZutatenAnz; i++) {
+       array[i] = document.getElementById("00"+(i+1)).innerHTML;
+       console.log(array[i]);
+   }
+    document.getElementById("DropDown").addEventListener('onchange', aendern());
+}
+
+function aendern(){
+    var neuDropDown = document.getElementById("DropDown").value;
+    for (var i = 0; i <= ZutatenAnz; i++) {
+        aktZutatenWert = array[i];
+        neuZutatenWert = ((aktZutatenWert/aktDropDown)* neuDropDown);
+        document.getElementById("00"+(i+1)).innerHTML = neuZutatenWert;
+    }
+    aktualisieren();
+}
 
 function addTableRow(){
 
